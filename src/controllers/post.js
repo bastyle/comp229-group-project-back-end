4cd347ccb403ea96062163cb1a419489727cbc2a
bastian bastias <bastian.bastias@gmail.com>
2022-11-24 21:40:40 -0500

    response

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -7,12 +7,13 @@ let Post = require('../models/post');
 module.exports.displayPostList = (req, res, next) => {
     console.log("controller::displayPostList");
     Post.find((err, postList) => {
-        console.log("finding..."+err)
+        console.log("err..."+err)
         if(err) {            
             return console.error(err);
         } else {
-            console.log("postList..."+postList)
-            res.json(postList);
+            console.log("postList..."+postList);
+            res.status(200).send(postList);
+            //res.json(postList);
         }
     });
 }
